e3246de99990f9edf8bf79bf12d459b9e33d3927
l <l>
2020-10-26 10:43:01 +0800

    添加路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,22 @@ import {TopBarComponent} from './top-bar/top-bar.component';
 import { RouterModule } from '@angular/router';
 import {ProductListComponent} from './product-list/product-list.component';
 import { ProductAlertComponent } from './product-alert/product-alert.component';
+import {ProductDetailsComponent} from './product-details/product.details.component'
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      {path:'',component:ProductListComponent}
+      {path:'',component:ProductListComponent},
+      {path:'products/:productId',component:ProductDetailsComponent}
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertComponent
+    ProductAlertComponent,
+    ProductDetailsComponent
   ],
   bootstrap: [ AppComponent ]
 })
